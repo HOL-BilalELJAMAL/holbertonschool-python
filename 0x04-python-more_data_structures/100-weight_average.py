d2db3656c8848619c88c38331f4b8023f40f01e7
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-01-26 15:55:55 +0000

    Add Task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if not my_list:
+        return 0
     sum, mult = 0, 0
     for i in range(len(my_list)):
         mult += my_list[i][0] * my_list[i][1]
