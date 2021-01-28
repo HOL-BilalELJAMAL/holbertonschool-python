9a9b775947b02fc1b8c0e229496e26ccfb0dc7a0
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-01-28 00:33:59 +0000

    Add Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             cnt += 1
-    except:
+    except (IndexError, TypeError):
         pass
     print()
     return cnt
