1c9063c1351648c99670ef7da1ce8a957d8e36e1
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-01-31 23:49:40 +0000

    Add Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    2-square.py
+    Module that defines a Square and return square size
+"""
+
+
+class Square:
+    """
+    Represents a class called Square with a private instance
+    attribute called size
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialization of the private instance attribute called size
+
+        Args:
+            size (int): Size of the Square
+
+        Note:
+            If size is not an integer, a TypeError exception is raised
+            Else If size is negative, a ValueError exception is raised
+            Otherwise, Successful Initialization
+        """
+        if type(size) == int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
