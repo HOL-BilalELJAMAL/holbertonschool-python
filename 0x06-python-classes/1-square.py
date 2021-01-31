462c376af671d492e5d5acb018128e1f26e3e5b2
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-01-31 23:50:20 +0000

    Add Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    1-square.py
+    Module that defines a Square and return empty dictionary
+"""
+
+
+class Square:
+    """
+    Represents a class called Square with a private instance
+    attribute called size
+    """
+
+    def __init__(self, size):
+        """Initialization of the private instance attribute called size"""
+        self.__size = size
