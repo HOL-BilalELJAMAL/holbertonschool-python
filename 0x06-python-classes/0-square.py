2855649529769d50f9163d5714c5eccec75ab588
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-01-31 23:19:33 +0000

    Add Task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    0-square.py
+    Module that defines a Square and return empty dictionnary
+"""
+
+
+class Square:
+    """Represents a minimal class called Square """
+    pass
