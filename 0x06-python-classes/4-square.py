4866c9e3d0e398c75edbb0aba3aa48ca96e212a5
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-02-01 00:27:25 +0000

    Add Task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+    4-square.py
+    Module that defines a Square and return square size
+    Including Getters and Setters
+    Including Method to calculate the Square's area
+"""
+
+
+class Square:
+    """
+    Represents a class called Square with a private instance
+    attribute called size
+    """
+    def __init__(self, size=0):
+        """
+        Initialization of the private instance attribute called size
+
+        Args:
+            size (int): Size of the Square
+
+        Note:
+            If size is not an integer, a TypeError exception is raised
+            Else If size is negative, a ValueError exception is raised
+            Otherwise, Successful Initialization
+        """
+        if type(size) == int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    @property
+    def size(self):
+        """Getter property to get the size of the Square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the Square
+
+        Args:
+            value (int): New size of the Square
+
+        Note:
+            If size is not an integer, a TypeError exception is raised
+            Else If size is negative, a ValueError exception is raised
+            Otherwise, Successful Set
+        """
+        if type(value) == int:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """Function that returns the area of the square"""
+        return self.__size ** 2
