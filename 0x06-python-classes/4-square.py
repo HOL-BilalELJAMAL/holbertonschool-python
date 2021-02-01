cf91c35074c0f07af94151133fe5887abb6f6a75
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-02-01 01:35:17 +0000

    Add Task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -13,24 +13,8 @@ class Square:
     attribute called size
     """
     def __init__(self, size=0):
-        """
-        Initialization of the private instance attribute called size
-
-        Args:
-            size (int): Size of the Square
-
-        Note:
-            If size is not an integer, a TypeError exception is raised
-            Else If size is negative, a ValueError exception is raised
-            Otherwise, Successful Initialization
-        """
-        if type(size) == int:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
-        else:
-            raise TypeError("size must be an integer")
+        """Initialization of the private instance attribute called size"""
+        self.__size = size
 
     @property
     def size(self):
