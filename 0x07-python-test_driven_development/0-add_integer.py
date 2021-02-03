4209418f98ae676f12321f9a09edb1ab3416832d
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-02-03 17:40:55 +0000

    Add Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -19,8 +19,8 @@ def add_integer(a, b=98):
         Else If b is not an integer or a float, a TypeError exception is raised
         Otherwise, cast a and b as integers and return their sum
     """
-    if not isinstance(a, int) and not isinstance(a, float):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int) and not isinstance(b, float):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
