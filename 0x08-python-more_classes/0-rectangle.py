bd246fc048b177aa1fbb52d5ed721162dfcc2fc8
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-02-07 23:37:59 +0000

    Add Task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    0-rectangle.py
+    Module that defines a Rectangle and returns empty dictionary
+"""
+
+
+class Rectangle:
+    """Represents a minimal class called Rectangle"""
+    pass
