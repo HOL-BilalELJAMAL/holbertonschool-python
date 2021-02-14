2f055dd5cb58adffbca1e81f4ffa0fba44d04adc
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-02-14 23:53:04 +0000

    Add Task 6

---
+++ 0x0B-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    6-base_geometry.py
+    Module that defines a BaseGeometry and return empty dictionary
+    Including Method to calculate the area
+"""
+
+
+class BaseGeometry:
+    """Represents a class called BaseGeometry"""
+
+    def area(self):
+        """Function that raises a specific exception"""
+        raise Exception("area() is not implemented")
