5404942cb18624e0af0c69b0988d13e73d0fbbd0
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-02-15 01:20:47 +0000

    Add Task 10

---
+++ 0x0B-python-inheritance/10-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    10-square.py
+    Module that defines a class called Square that inherits from
+    class Rectangle and returns its size
+    Include a method to calculate the Square's area
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Represents a class called Rectangle with a private instance
+    attribute called size
+    """
+
+    def __init__(self, size):
+        """
+        Initialization of the private instance attribute
+        Size will be validated using the integer_validator
+        implemented in the base class
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Function that returns the area of the rectangle"""
+        return self.__size ** 2
