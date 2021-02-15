d8394bc7503205b9161b7348709dc5ac56acca26
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-02-15 23:30:01 +0000

    Add Task 2

---
+++ 0x0C-python-input_output/2-read_lines.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    2-read_lines.py
+    Module that defines a function called read_lines that
+    reads n lines of a text file (UTF8) and prints it to stdout
+"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """
+    Function that prints n lines of a text file to stdout
+
+    Args:
+        filename: File name
+        nb_lines: Number of lines
+
+    Notes:
+        if nb_lines <= 0 or nb_lines > file number of lines
+            Prints entire file
+        Otherwise
+            Prints nb_lines lines of the file
+    """
+    cnt = 0
+    with open(filename) as f:
+        for line in f:
+            print(line, end="")
+            cnt += 1
+            if cnt == nb_lines and nb_lines > 0:
+                break
