327bd46e441d15bee389e3fd46962e022f6daf49
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-02-16 20:33:59 +0000

    Add Task 3

--- 0x0C-python-input_output/3-write_file.py
+++ 0x0C-python-input_output/3-write_file.py
@@ -18,6 +18,10 @@ def write_file(filename="", text=""):
     Notes:
         If file does not exist, file should be created
         If file already exists, overwrite the content of the file
-    """
     with open(filename, 'w') as f:
         return f.write(text)
+    """
+    with open(filename, 'w') as f:
+        chars = f.write(text)
+    return chars
+
