7dc4ab1398b0a07727d67232650435d336d9cc74
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-02-16 00:12:46 +0000

    Add Task 5

---
+++ 0x0C-python-input_output/5-to_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    5-to_json_string.py
+    Module that defines a function called to_json_string that returns
+    the JSON representation of an object (string)
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Function that returns the JSON representation of an object
+
+    Args:
+        my_obj: Object
+    """
+    return json.dumps(my_obj)
