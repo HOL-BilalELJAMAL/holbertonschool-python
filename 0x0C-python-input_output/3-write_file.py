2071c7b6540fd02031b6f67b0a90320eaf9539dc
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-02-16 16:13:34 +0000

    Add Task 3

--- 0x0C-python-input_output/3-write_file.py
+++ 0x0C-python-input_output/3-write_file.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
-"""module write_file"""
+"""
+    3-write_file.py
+    Module that defines a function called write_file that writes a string
+    to a text file (UTF8) and returns the number of characters written
+"""
 
 
 def write_file(filename="", text=""):
-    """function write_file"""
+    """
+    Function that writes a string to a text file and returns
+    the number of characters written
 
-    with open(filename, "w+") as f:
+    Args:
+        filename (file): File name
+        text (str): Text to be added to the file
+
+    Notes:
+        If file does not exist, file should be created
+        If file already exists, overwrite the content of the file
+    """
+    with open(filename, 'w') as f:
         return f.write(text)
