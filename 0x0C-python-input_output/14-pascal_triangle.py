2d213c5125e0a4ce3eb71b81afed30d3949be441
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-02-16 21:08:27 +0000

    Add Task 14

--- 0x0C-python-input_output/14-pascal_triangle.py
+++ 0x0C-python-input_output/14-pascal_triangle.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
+
 """
-    14-pascal_triangle.py
-    Module that defines a method called pascal_triangle that
-    returns a list of lists of integers representing the Pascals
-    triangle of height n
+pascal_triangle Module
 """
 
 
 def pascal_triangle(n):
-    """
-    Function that returns a list of lists of integers representing the Pascals
-    triangle of height n
-
-    Args:
-        n (int): Pascal's triangle height
-
-    Note:
-        if n<=0, return empty list of list
-        Otherwise, return Pascal's triangle in list of list
-    """
-    triangle = []
-    for i in range(n):
+    """Returns a list of lists of integers of n"""
+    res = []
+    if n <= 0:
+        return res
+    for x in range(n):
         row = [1]
-        if i > 0:
-            for j in range(i):
-                row.append(sum(triangle[-1][j:j + 2]))
-        triangle.append(row)
-    return triangle
+        if x > 0:
+            for j in range(x):
+                row.append(sum(res[-1][j:j + 2]))
+        res.append(row)
+    return res
