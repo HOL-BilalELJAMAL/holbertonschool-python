3ce72b180365fbd063741ac9144663a852b52e85
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-03-01 10:26:24 +0000

    Add Task 3

--- 0x10-python-network_1/3-error_code.py
+++ 0x10-python-network_1/3-error_code.py
@@ -10,7 +10,7 @@ from sys import argv
 
 if __name__ == "__main__":
     try:
-        with urllib.request.urlopen(argv) as f:
+        with urllib.request.urlopen(argv[1]) as f:
             print(f.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
         error_code = str(e).split(' ')[2][:-1]
