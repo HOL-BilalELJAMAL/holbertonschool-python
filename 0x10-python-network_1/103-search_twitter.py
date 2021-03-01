c37873befefde0b00299fd9433f4adee0abfd525
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-03-01 16:28:43 +0000

    Add Task 11

---
+++ 0x10-python-network_1/103-search_twitter.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""
+103-search_twitter.py
+Module that displays the first 5 tweets based on specific search result
+using this format [<Tweet ID>] <Tweet text> by <Tweet owner name>
+"""
