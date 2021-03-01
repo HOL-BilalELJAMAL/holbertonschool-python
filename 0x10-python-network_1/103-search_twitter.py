7d23565d218c79bc2e6605ccabf3210b7b764a3d
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-03-01 13:55:30 +0000

    Add Task 11

---
+++ 0x10-python-network_1/103-search_twitter.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+103-search_twitter.py
+Module that displays the first 5 tweets based on specific search result
+using this format [<Tweet ID>] <Tweet text> by <Tweet owner name>
+"""
+import base64
+import requests
+from sys import argv
+
+client_key = argv[1]
+client_secret = argv[2]
+key_secret = '{}:{}'.format(client_key, client_secret).encode('ascii')
+
+b64_encoded_key = base64.b64encode(key_secret)
+b64_encoded_key = b64_encoded_key.decode('ascii')
+
+base_url = 'https://api.twitter.com/'
+auth_url = '{}oauth2/token'.format(base_url)
+auth_headers = {
+    'Authorization': 'Basic {}'.format(b64_encoded_key),
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+}
+auth_data = {
+    'grant_type': 'client_credentials'
+}
+auth_resp = requests.post(auth_url, headers=auth_headers, data=auth_data)
+
+access_token = auth_resp.json()['access_token']
+
+search_headers = {
+    'Authorization': 'Bearer {}'.format(access_token)
+}
+
+search_params = {
+    'q': argv[3],
+    'result_type': 'recent',
+    'count': 5
+}
+
+search_url = '{}1.1/search/tweets.json'.format(base_url)
+search_resp = requests.get(search_url, headers=search_headers,
+                           params=search_params)
+
+data = search_resp.json()
+for obj in data:
+    print("[{}] {} by {}".format(obj.get('id'),
+                                 obj.get('text'),
+                                 obj.get('user').get('name')))
