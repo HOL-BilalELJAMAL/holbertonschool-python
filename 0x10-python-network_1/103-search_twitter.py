416be892380d37ae1f8c9e1c702b183a4477f4ca
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-03-01 16:33:54 +0000

    Add Task 11

--- 0x10-python-network_1/103-search_twitter.py
+++ 0x10-python-network_1/103-search_twitter.py
@@ -4,3 +4,11 @@
 Module that displays the first 5 tweets based on specific search result
 using this format [<Tweet ID>] <Tweet text> by <Tweet owner name>
 """
+
+import requests
+import base64
+import sys
+
+client_key = sys.argv[1]
+client_secret = sys.argv[2]
+search_str = sys.argv[3]
