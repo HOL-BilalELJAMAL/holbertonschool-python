6fb91f73843954b2a0b395d97594f0c215b236fa
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-03-03 02:03:23 +0000

    Add Task 4

--- 0x12-SQL_introduction/4-first_table.sql
+++ 0x12-SQL_introduction/4-first_table.sql
@@ -1,2 +1,4 @@
 -- Script that creates a table called first_table in the current database in your MySQL server
-CREATE TABLE first_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(256));
