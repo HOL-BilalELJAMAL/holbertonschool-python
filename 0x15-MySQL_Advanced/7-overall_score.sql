33ffc8315bfe92d9eb1aff7eeefd1a64e9f1c520
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-03-10 09:57:43 +0000

    Add Task 7

---
+++ 0x15-MySQL_Advanced/7-overall_score.sql
@@ -0,0 +1,9 @@
+-- Script that creates a stored procedure ComputeOverallScoreForUser that computes and store the overall score for a student
+DELIMITER |
+CREATE PROCEDURE ComputeOverallScoreForUser (IN user_id int)
+BEGIN
+UPDATE users AS u set u.overall_score = (
+SELECT FLOOR(SUM(c.score)/COUNT(c.score)) FROM corrections AS c where c.user_id = user_id
+) WHERE u.id = user_id;
+END;
+|
