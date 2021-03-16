9d16a708994851fdd5bd7562d998654c30dde929
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-03-16 23:14:58 +0000

    Add Task 0

--- 0x16-count_it/0-count.py
+++ 0x16-count_it/0-count.py
@@ -28,18 +28,18 @@ def count_words(subreddit, word_list, res=defaultdict(int), after=None):
     if after:
         url += '?after={}'.format(after)
     try:
-        r = requests.get(url, headers=headers, allow_redirects=False).json()
-        titles = r.get('data').get('children')
-        for t in titles:
-            c = Counter(t.get('data').get('title').lower().split(' '))
-            for word in word_list:
-                if word.lower() in c:
-                    res[word] += c.get(word.lower())
-        after = r.get('data').get('after')
+        req = requests.get(url, headers=headers, allow_redirects=False).json()
+        titles = req.get('data').get('children')
+        for title in titles:
+            count = Counter(title.get('data').get('title').lower().split(' '))
+            for x in word_list:
+                if x.lower() in count:
+                    res[x] += count.get(x.lower())
+        after = req.get('data').get('after')
         if after:
             return count_words(subreddit, word_list, res, after)
-        first_sort = sorted(res.items(), key=lambda x: x[0])
-        for k, v in sorted(first_sort, key=lambda x: x[1], reverse=True):
+        sort_first = sorted(res.items(), key=lambda x: x[0])
+        for k, v in sorted(sort_first, key=lambda x: x[1], reverse=True):
             print('{}: {}'.format(k, v))
     except:
         return
