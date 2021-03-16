81c11f462c6c7314073ef65be6f771b8f030044f
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-03-16 23:21:23 +0000

    Add Task 0

--- 0x16-count_it/0-count.py
+++ 0x16-count_it/0-count.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-0-count.py
-Module that defines a recursive function that queries the Reddit API
+Recursive function that queries the reddit API,
+parses the title of all the hot articles,
+prints a sorted count of a given set of keywords
 """
 
 from collections import Counter, defaultdict
@@ -10,16 +11,7 @@ import requests
 
 
 def count_words(subreddit, word_list, res=defaultdict(int), after=None):
-    """
-    Recursive function that queries the Reddit API, parses the title of all
-    hot articles, and prints a sorted count of given keywords
-
-    Args:
-        subreddit (str): Subreddit to search
-        word_list (list): List of words to search
-        res (dict): Pairs of words - counts
-        after (str): Parameter for the next page of the API result
-    """
+    """Counting the words in Subreddit"""
     agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6)\
             AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132i\
             Safari/537.36"
