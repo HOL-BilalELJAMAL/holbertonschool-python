9851edcc95897a06a5d97da21bf2484c012876fd
Bilal EL JAMAL <bilal.eljamal@holcomgroup.com>
2021-03-31 21:07:50 +0000

    Add Task 0

--- 0x1D-primegame/0-prime_game.py
+++ 0x1D-primegame/0-prime_game.py
@@ -1,55 +1,39 @@
 #!/usr/bin/python3
+
 """
-0-prime_game.py
-Module that defines the winner of the prime game between 2 players
+Module for isWinner.
 """
-
-
-def isPrime(n):
-    """
-    Function that determines if an integer is prime
-
-    Args:
-        n (int): Integer
-    """
-    if n <= 1:
-        return False
-    for i in range(2, (n // 2) + 1):
-        if n % i == 0:
-            return False
-    return True
 
 
 def isWinner(x, nums):
-    """
-    Function that determines the winner of the game
-
-    Args:
-        x (int): Number of rounds
-        nums (list): List of integers
-
-    Notes:
-        If the winner cannot be determined, return None
-        Otherwise, return the name of the player that won the most rounds
-    """
-    p1, p2 = 0, 0
-    for i in range(x):
-        for j in range(len(nums)):
-            if isPrime(nums[j]):
-                temp = nums[j]
-                nums.pop(j)
-                for k in nums:
-                    if k % temp == 0:
-                        nums.remove(k)
-                if i % 2 == 0:
-                    p1 += 1
-                else:
-                    p2 += 1
-                break
-            if j + 1 == len(nums) and p1 == 0 and p2 == 0:
-                return None
-    if p1 == p2:
+    """ Prime numbers game. """
+    if x < 0 or nums[-1] < 1:
         return None
-    if p1 > p2:
+    Maria_score = 0
+    Ben_score = 0
+    prime = [False] * 100001
+    primes = []
+    for i in range(2, 100001):
+        if not prime[i]:
+            primes.append(i)
+            for j in range(i, 100001, i):
+                prime[j] = True
+
+    l = 0
+    for a0 in range(x):
+        n = nums[l]
+        # your code goes here
+        num = 0
+        for i in range(100001):
+            if i >= len(primes) or primes[i] > n:
+                break
+            num += 1
+        l += 1
+        if (['Maria', 'Ben'][(num % 2) ^ 1]) == "Maria":
+            Maria_score += 1
+        else:
+            Ben_score += 1
+    if Maria_score > Ben_score:
         return "Maria"
-    return "Ben"
+    else:
+        return "Ben"
